a10d38be2b235c001507b58aa4544e3cb805007b
gardenlee <khjvvv7@gmail.com>
2023-04-24 22:31:49 +0900

    hello wequiz

---
+++ src/main/kotlin/kr/mashup/wequiz/WequizApplication.kt
@@ -0,0 +1,11 @@
+package kr.mashup.wequiz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WequizApplication
+
+fun main(args: Array<String>) {
+	runApplication<WequizApplication>(*args)
+}
