5c7f50d3fefef1df3c51bebc4fc0721321f76010
gardenlee <garden.lee@toss.im>
2023-06-09 21:18:44 +0900

    퀴즈 리스트

--- src/main/kotlin/kr/mashup/wequiz/controller/quiz/QuizApiController.kt
+++ src/main/kotlin/kr/mashup/wequiz/controller/quiz/QuizApiController.kt
@@ -3,16 +3,14 @@ package kr.mashup.wequiz.controller.quiz
 import kr.mashup.wequiz.application.quiz.QuizService
 import kr.mashup.wequiz.config.auh.UserInfo
 import kr.mashup.wequiz.config.auh.UserInfoDto
-import kr.mashup.wequiz.controller.quiz.model.CreateQuizRequest
-import kr.mashup.wequiz.controller.quiz.model.CreateQuizResponse
-import kr.mashup.wequiz.controller.quiz.model.DeleteQuizResponse
-import kr.mashup.wequiz.controller.quiz.model.GetQuizResponse
+import kr.mashup.wequiz.controller.quiz.model.*
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -34,6 +32,16 @@ class QuizApiController(
         @PathVariable(name = "quizId") quizId: Long,
     ): GetQuizResponse {
         return quizService.getQuiz(quizId)
+    }
+
+    @GetMapping
+    fun getQuizList(
+        @UserInfo userInfoDto: UserInfoDto,
+        @RequestParam size: Int = 15,
+        @RequestParam cursor: Long? = null,
+    ): GetQuizListResponse {
+        val quizList = quizService.getQuizList(cursor, size)
+        return GetQuizListResponse.from(quizList)
     }
 
     @DeleteMapping("/{quizId}")
---
+++ src/main/kotlin/kr/mashup/wequiz/controller/quiz/model/GetQuizListModel.kt
@@ -0,0 +1,27 @@
+package kr.mashup.wequiz.controller.quiz.model
+
+import kr.mashup.wequiz.domain.quiz.Quiz
+
+data class GetQuizListResponse(
+    val quiz: List<QuizDto>,
+    val nextCursor: Long?,
+) {
+    data class QuizDto(
+        val id: Long,
+        val title: String,
+    )
+
+    companion object {
+        fun from(quizList: List<Quiz>): GetQuizListResponse {
+            return GetQuizListResponse(
+                quiz = quizList.map {
+                    QuizDto(
+                        id = it.id,
+                        title = it.title,
+                    )
+                },
+                nextCursor = quizList.lastOrNull()?.id
+            )
+        }
+    }
+}
