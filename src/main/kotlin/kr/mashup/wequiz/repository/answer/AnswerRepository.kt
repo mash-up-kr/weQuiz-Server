6411f9d570c70e51f7d08124d02b6003195df64a
Heidi <heidi@daangn.com>
2023-06-17 18:27:43 +0900

    기존 Answer는 제거

--- src/main/kotlin/kr/mashup/wequiz/repository/answer/AnswerRepository.kt
+++
@@ -1,6 +0,0 @@
-package kr.mashup.wequiz.repository.answer
-
-import kr.mashup.wequiz.domain.answer.Answer
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AnswerRepository : JpaRepository<Answer, Long>
