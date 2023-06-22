9005f8b8def515e1e753104850df7eb9bf13eb3e
gardenlee <garden.lee@toss.im>
2023-06-22 21:25:27 +0900

    스웨거 설정

---
+++ src/main/kotlin/kr/mashup/wequiz/config/swagger/SwaggerConfig.kt
@@ -0,0 +1,42 @@
+package kr.mashup.wequiz.config.swagger
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityScheme
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import org.springdoc.core.models.GroupedOpenApi
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@OpenAPIDefinition(
+    info = Info(title = "WeQuiz API", description = "토큰 발급 후 사용 하시기 바랍니다.")
+)
+@SecurityScheme(
+    type = SecuritySchemeType.APIKEY, `in` = SecuritySchemeIn.HEADER,
+    name = "x-wequiz-token", description = "Auth Token",
+)
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun weQuizApiV1(): GroupedOpenApi {
+        val paths = arrayOf("/api/v1/**")
+        return GroupedOpenApi.builder()
+            .group("WeQuiz API v1")
+            .addOpenApiCustomizer {
+                it.security(
+                    listOf(
+                        SecurityRequirement().addList(TOKEN_NAME),
+                    )
+                )
+            }
+            .pathsToMatch(*paths)
+            .build()
+    }
+
+    companion object {
+        private const val TOKEN_NAME = "x-wequiz-token"
+    }
+}
--- src/main/kotlin/kr/mashup/wequiz/controller/user/UserApiController.kt
+++ src/main/kotlin/kr/mashup/wequiz/controller/user/UserApiController.kt
@@ -1,5 +1,7 @@
 package kr.mashup.wequiz.controller.user
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import kr.mashup.wequiz.application.user.UserService
 import kr.mashup.wequiz.controller.user.model.JoinUserRequest
 import org.springframework.web.bind.annotation.PostMapping
@@ -7,11 +9,13 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "유저 API", description = "UserApiController")
 @RestController
 @RequestMapping("/api/v1/user")
 class UserApiController(
     private val userService: UserService
 ) {
+    @Operation(summary = "회원 가입")
     @PostMapping("/join")
     fun joinUser(
         @RequestBody joinUserRequest: JoinUserRequest
