687946f29d8eee6364f749ba4cec9ea652991b24
Heidi <heidi@daangn.com>
2023-07-01 20:51:50 +0900

    랭킹 페이지네이션 서비스 & 레포지토리 추가

--- src/main/kotlin/kr/mashup/wequiz/repository/answer/QuizAnswerRepository.kt
+++ src/main/kotlin/kr/mashup/wequiz/repository/answer/QuizAnswerRepository.kt
@@ -1,8 +1,21 @@
 package kr.mashup.wequiz.repository.answer
 
 import kr.mashup.wequiz.domain.answer.QuizAnswer
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface QuizAnswerRepository : JpaRepository<QuizAnswer, Long> {
     fun findAllByQuizId(quizId: Long): List<QuizAnswer>
+
+    fun findAllByQuizIdAndTotalScoreLessThanAndIdLessThanAndDeletedAtIsNullOrderByTotalScoreDescCreatedAtDesc(
+        quizId: Long,
+        totalScore: Long, // 커서
+        id: Long, // 커서
+        pageable: Pageable
+    ): List<QuizAnswer>
+
+    fun findAllByQuizIdAndDeletedAtIsNullOrderByTotalScoreDescCreatedAtDesc(
+        quizId: Long,
+        pageable: Pageable
+    ): List<QuizAnswer>
 }
