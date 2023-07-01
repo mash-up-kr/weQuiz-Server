756bdbacd07900e4c2dc5003f22a7f24361bd7bd
gardenlee <garden.lee@toss.im>
2023-07-01 17:23:17 +0900

    헬스 체크 API

---
+++ src/main/kotlin/kr/mashup/wequiz/controller/HealthController.kt
@@ -0,0 +1,14 @@
+package kr.mashup.wequiz.controller
+
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Tag(name = "Health Check", description = "HealthController")
+@RestController
+class HealthController {
+    @GetMapping("/health")
+    fun health(): String {
+        return "SUCCESS"
+    }
+}
