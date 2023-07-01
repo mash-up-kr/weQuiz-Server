3fe1147f18f85188579fb0d81b0718d7740249c9
Heidi <heidi@daangn.com>
2023-07-01 20:51:32 +0900

    Transient 설정 & isDelete error 제거

--- src/main/kotlin/kr/mashup/wequiz/domain/quiz/question/Question.kt
+++ src/main/kotlin/kr/mashup/wequiz/domain/quiz/question/Question.kt
@@ -47,6 +47,7 @@ class Question(
     val deletedAt: LocalDateTime? = null
 ) {
 
+    @jakarta.persistence.Transient
     val answersCount = options.sumOf { it.isCorrect.toInt() }
 
     fun setOptions(options: List<Option>) {
