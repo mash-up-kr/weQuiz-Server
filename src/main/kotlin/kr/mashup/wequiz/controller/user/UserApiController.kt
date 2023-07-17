875b4861c4e9d6069ba71ec3eb3c441d678b3717
Heidi <heidi@daangn.com>
2023-07-18 02:16:37 +0900

    내 정보 조회 API 작성

--- src/main/kotlin/kr/mashup/wequiz/controller/user/UserApiController.kt
+++ src/main/kotlin/kr/mashup/wequiz/controller/user/UserApiController.kt
@@ -1,9 +1,13 @@
 package kr.mashup.wequiz.controller.user
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.tags.Tag
 import kr.mashup.wequiz.application.user.UserService
+import kr.mashup.wequiz.config.auh.UserInfo
+import kr.mashup.wequiz.config.auh.UserInfoDto
 import kr.mashup.wequiz.controller.user.model.JoinUserRequest
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -27,4 +31,24 @@ class UserApiController(
             description = joinUserRequest.description
         )
     }
+
+    @Operation(summary = "내 정보 조회하기")
+    @GetMapping()
+    fun me(
+        @Schema(hidden = true) @UserInfo userInfoDto: UserInfoDto
+    ): UserPresentation {
+        val user = userService.find(userInfoDto.id)
+
+        return UserPresentation(
+            id = user.id,
+            nickname = user.nickname,
+            description = user.description
+        )
+    }
+
+    data class UserPresentation(
+        val id: Long,
+        val nickname: String,
+        val description: String
+    )
 }
