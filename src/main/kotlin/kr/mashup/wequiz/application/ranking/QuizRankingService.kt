91c6823f7f3e205b2aa4f8b06156c3f1aa76a138
Heidi <heidi@daangn.com>
2023-07-17 23:57:54 +0900

    퀴즈랭킹 서비스 작성하기

--- src/main/kotlin/kr/mashup/wequiz/application/ranking/QuizRankingService.kt
+++ src/main/kotlin/kr/mashup/wequiz/application/ranking/QuizRankingService.kt
@@ -1,27 +1,53 @@
 package kr.mashup.wequiz.application.ranking
 
-import kr.mashup.wequiz.domain.answer.QuizAnswer
+import kr.mashup.wequiz.application.answer.QuizAnswerRankingDto
+import kr.mashup.wequiz.application.answer.toRankingDto
+import kr.mashup.wequiz.repository.answer.QuizAnswerQueryRepository
 import kr.mashup.wequiz.repository.answer.QuizAnswerRepository
-import org.springframework.data.domain.PageRequest
+import kr.mashup.wequiz.repository.answer.SortOrder
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class QuizRankingService(
-    private val quizAnswerRepository: QuizAnswerRepository
+    private val quizAnswerRepository: QuizAnswerRepository,
+    private val quizAnswerQueryRepository: QuizAnswerQueryRepository
 ) {
 
-    fun getQuizAnswerRanking(quizId: Long, size: Int, cursor: Long? = null): List<QuizAnswer> {
-        return if (cursor == null) {
-            quizAnswerRepository.findAllByQuizIdAndDeletedAtIsNullOrderByTotalScoreDescCreatedAtDesc(quizId, PageRequest.of(0, size))
+    @Transactional(readOnly = true)
+    fun findQuizRanking(quizAnswerId: Long?, quizId: Long, size: Int): List<QuizAnswerRankingDto> {
+        val cursorScore = if (quizAnswerId != null) {
+            quizAnswerRepository.findByIdOrNull(quizAnswerId)?.totalScore
+                ?: throw throw IllegalArgumentException("$quizId 에 해당하는 정답지가 없어요")
         } else {
-            val quizAnswer = quizAnswerRepository.findByIdOrNull(cursor) ?: throw RuntimeException("")
-            quizAnswerRepository.findAllByQuizIdAndTotalScoreLessThanAndIdLessThanAndDeletedAtIsNullOrderByTotalScoreDescCreatedAtDesc(
-                quizId = quizId,
-                totalScore = quizAnswer.totalScore.toLong(),
-                id = cursor,
-                pageable = PageRequest.of(0, size)
-            )
+            Int.MAX_VALUE
         }
+
+        return quizAnswerQueryRepository.findQuizAnswersByQuizId(
+            quizAnswerId = quizAnswerId,
+            cursorScore = cursorScore,
+            quizId = quizId,
+            size = size,
+            sortOrder = SortOrder.TOTAL_SCORE_DESC
+        ).map { it.toRankingDto() }
+    }
+
+    @Transactional(readOnly = true)
+    fun findMyQuizRanking(quizAnswerId: Long?, quizCreatorId: Long, size: Int): List<QuizAnswerRankingDto> {
+        val cursorScore = if (quizAnswerId != null) {
+            quizAnswerRepository.findByIdOrNull(quizAnswerId)?.totalScore
+                ?: throw throw IllegalArgumentException("$quizAnswerId 에 해당하는 정답지가 없어요")
+        } else {
+            Int.MAX_VALUE
+        }
+
+        return quizAnswerQueryRepository.findQuizAnswersByQuizCreatorId(
+            quizAnswerId = quizAnswerId,
+            cursorScore = cursorScore,
+            quizCreatorId = quizCreatorId,
+            size = size,
+            sortOrder = SortOrder.TOTAL_SCORE_DESC
+        ).map { it.toRankingDto() }
     }
 }
