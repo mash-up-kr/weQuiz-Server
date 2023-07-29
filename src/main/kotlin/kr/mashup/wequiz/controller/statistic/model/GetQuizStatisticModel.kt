c85acbf9888bd26c64cab80a49f29ccbefc13abe
Heidi <63635886+yaeoni@users.noreply.github.com>
2023-07-29 13:53:50 +0900

    통계 api 응답 스펙 변경 (#38)

--- src/main/kotlin/kr/mashup/wequiz/controller/statistic/model/GetQuizStatisticModel.kt
+++ src/main/kotlin/kr/mashup/wequiz/controller/statistic/model/GetQuizStatisticModel.kt
@@ -4,60 +4,43 @@ package kr.mashup.wequiz.controller.statistic.model
 import kr.mashup.wequiz.domain.statistic.QuizStatistic
 
 data class GetQuizStatisticResponse(
-    val quizInfo: QuizInfo
+    val quizId: Long,
+    val quizTitle: String,
+    val questions: List<QuestionStatisticsPresentation>
 ) {
     companion object {
         fun from(quizStatistic: QuizStatistic): GetQuizStatisticResponse {
             return GetQuizStatisticResponse(
-                quizInfo = QuizInfo(
-                    quizId = quizStatistic.quizView.id,
-                    quizTitle = quizStatistic.quizView.title,
-                    questions = quizStatistic.quizView.questions.map { question ->
-                        QuestionDto(
-                            questionId = question.id,
-                            questionTitle = question.title,
-                            options = question.options.map { option ->
-                                OptionDto(
-                                    optionId = option.id,
-                                    content = option.content,
-                                    isCorrect = option.isCorrect,
-                                    selectivity = quizStatistic.findOptionStatistic(question.id, option.id)?.selectivity ?: 0f
-                                )
-                            }
-                        )
-                    }
-                )
+                quizId = quizStatistic.quizView.id,
+                quizTitle = quizStatistic.quizView.title,
+                questions = quizStatistic.quizView.questions.map { question ->
+                    QuestionStatisticsPresentation(
+                        questionId = question.id,
+                        questionTitle = question.title,
+                        options = question.options.map { option ->
+                            OptionStatisticsPresentation(
+                                optionId = option.id,
+                                content = option.content,
+                                isCorrect = option.isCorrect,
+                                selectivity = quizStatistic.findOptionStatistic(question.id, option.id)?.selectivity ?: 0f
+                            )
+                        }
+                    )
+                }
             )
         }
     }
 
-    data class QuizInfo(
-        val quizId: Long,
-        val quizTitle: String,
-        val questions: List<QuestionDto>
-    )
-
-    data class QuestionDto(
+    data class QuestionStatisticsPresentation(
         val questionId: Long,
         val questionTitle: String,
-        val options: List<OptionDto>
+        val options: List<OptionStatisticsPresentation>
     )
 
-    data class OptionDto(
+    data class OptionStatisticsPresentation(
         val optionId: Long,
         val content: String,
         val isCorrect: Boolean,
         val selectivity: Float
-    )
-
-    data class QuestionStatisticDto(
-        val questionId: Long,
-        val options: List<OptionStatisticDto>
-    )
-
-    data class OptionStatisticDto(
-        val optionId: Long,
-        val selectivity: Float,
-        val isCorrect: Boolean
     )
 }
