899e7ff5e94003d2429328b1ea54ab25bdafa7c0
GardenLee <khjvvv7@gmail.com>
2023-07-29 16:41:52 +0900

    퀴즈 생성 응답 수정 (#49)
    Co-authored-by: gardenlee <garden.lee@toss.im>

--- src/main/kotlin/kr/mashup/wequiz/domain/exception/WeQuizError.kt
+++ src/main/kotlin/kr/mashup/wequiz/domain/exception/WeQuizError.kt
@@ -6,5 +6,7 @@ enum class WeQuizError(val code: String, val description: String = "") {
     WEC400("WEC400", "토큰이 없습니다."),
     WEC401("WEC401", "유효하지 않은 토큰 입니다."),
 
+    WEC500("WEC500", "이미 회원 가입한 유저에요."),
+
     WEC999("WEC999", "문제가 발생 했어요")
 }
